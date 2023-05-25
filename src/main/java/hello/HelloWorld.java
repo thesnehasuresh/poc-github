d3a97e1727fc15cc977995c4ef50fa5be871df93
thesnehasuresh <85167858+thesnehasuresh@users.noreply.github.com>
2023-05-25 20:23:49 +0530

    Testing the application HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,15 +1,13 @@
-package com.example;
+package hello;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+public class Greeter {
 
-public class HelloWorldServlet extends HttpServlet {
+    public String sayHello() {
+        return "Hello world!";
+    }
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        response.getWriter().println("<h1>Hello, World!</h1>");
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
     }
 }
