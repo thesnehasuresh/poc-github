2c3829b51ae09618e24d68b2bbc7c217f9f817a7
thesnehasuresh <85167858+thesnehasuresh@users.noreply.github.com>
2023-05-25 20:07:37 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,8 +1,15 @@
-package hello;
+package com.example;
 
-public class HelloWorld {
-	public static void main(String[] args) {
-		Greeter greeter = new Greeter();
-		System.out.println(greeter.sayHello());
-	}
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello, World!</h1>");
+    }
 }
